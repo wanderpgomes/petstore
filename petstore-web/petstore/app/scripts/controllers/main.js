8517c70d4f50cc43da747dd65e10ee0452f8479c
Wander Gomes <wander.gomes@gmail.com>
2015-11-29 18:15:49 -0500

    Add table with list of pets.

--- petstore-web/petstore/app/scripts/controllers/main.js
+++ petstore-web/petstore/app/scripts/controllers/main.js
@@ -12,11 +12,36 @@ angular.module('petstoreApp').controller('MainCtrl', function ($scope, $http) {
     $scope.name = '';
     $scope.status = '';
 
+    $scope.pets = [];
+
     $scope.addPet = function(){
-    	$http.post('http://localhost:8080/pet', {name: $scope.name, status: $scope.status}).then(function successCallback(response) {
-		    
-		 }, function errorCallback(response) {
+    	if (name && status){
+	    	$http.post('http://localhost:8080/pet', {name: $scope.name, status: $scope.status}).then(function success(response) {
+
+	    		$scope.clearForm();
+	    		$scope.getPets();
+			    
+			 }, function error(response) {
+			    
+			 });
+		}
+    };
+
+    $scope.getPets = function(){
+    	$http.get('http://localhost:8080/pet').then(function success(response) {
+		 	
+		 	$scope.pets = response.data;
+
+		 }, function error(response) {
 		    
 		 });
     };
+
+    $scope.clearForm =  function() {
+    	$scope.name = '';
+    	$scope.status = '';
+    };
+
+    $scope.getPets();
+
   });
